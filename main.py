0346d532e3f5fc457d29b3ca335fde3da543ade5
HimanshuSuman7 <himanshu.suman1010@gmail.com>
2019-09-15 17:34:17 +0530

    Changes to the code
    Better accuracy.

--- main.py
+++ main.py
@@ -1,45 +1,59 @@
-import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
-# Generate random values
-# x = np.random.random_integers(1000, 1700, 100)
-# y = np.random.random_integers(1000, 1700, 100)
-dataset = pd.read_csv("linear_dataset.csv", sep=',', header=None).to_numpy()
+# Reading data
+dataset = pd.read_csv("linear_dataset.csv", header=None).to_numpy()
+# print(dataset.shape)
+# print(dataset.head())
+
 x = dataset[..., 0:1]
+# print(x)
 y = dataset[..., 1:2]
+# print(y)
 
-# Calculate mean for x & y
 mean_x = np.mean(x)
 mean_y = np.mean(y)
+# print(mean_x, mean_y)
 
-# Required declaration
 total_values = len(x)
-var1 = 0
-var2 = 0
 
-# Implement simple linear equation formula
+num, den = 0, 0
 for i in range(total_values):
-    var1 += (x[i] - mean_x) * (y[i] - mean_y)
-    var2 += (x[i] - mean_x) ** 2
+    num += (x[i] - mean_x) * (y[i] - mean_y)
+    den += (x[i] - mean_x) ** 2
 
-# Known formula to calculate slope and const
-slope = var1 / var2
-const = mean_y - (slope * mean_x)
-print(slope, const)
+slope = num / den
+const = mean_y - slope * mean_x
 
-# To plot the graph
-max_value = np.max(x)
-min_value = np.min(x)
+print("Slope = {}".format(slope), "Intercept = {}".format(const))
 
-# Here, pass a few points to draw the straight line
-a = np.linspace(min_value, max_value, 1000)
+a = np.linspace(np.min(x), np.max(x), 1000)
 b = const + slope * a
 
-# Plot points and line
-plt.plot(a, b, color="#58b970", label="Regression line")
-plt.scatter(x, y, c='#ef5423', label="Scatter points")
-plt.xlabel("Independent")
-plt.ylabel("Dependent")
+plt.plot(a, b, color='RED', label='Regression line')
+plt.scatter(x, y, color='BLUE', label='Scatter plot')
+plt.xlabel('Study Hours')
+plt.ylabel('Marks')
 plt.legend()
 plt.show()
+
+# Predicted values
+y_pred = const + slope * x
+
+# Root-mean-squared-error
+rmse = 0
+for i in range(total_values):
+    rmse += (y_pred[i] - y[i]) ** 2
+
+rmse = np.sqrt(rmse / total_values)
+print("Root mean squared error : {}".format(rmse))
+
+# Evaluating R2 Score
+ss_t, ss_r = 0, 0
+for i in range(total_values):
+    ss_t += (y[i] - mean_y) ** 2
+    ss_r += (y[i] - y_pred[i]) ** 2
+
+r2_score = 1 - (ss_r / ss_t)
+print("R2 Score : {}".format(r2_score))
